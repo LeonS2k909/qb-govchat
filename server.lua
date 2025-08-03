9100e702d0be135e5a8d3efde50c8d97530983f7
LeonS2k909 <leon2k909@outlook.com>
2025-08-03 05:10:29 +0100

    /broadcast to announce a message to whole server

--- server.lua
+++ server.lua
@@ -40,3 +40,35 @@ RegisterCommand("gov", function(source, args)
         end
     end
 end, false)
+
+RegisterCommand("broadcast", function(source, args)
+    local Player = QBCore.Functions.GetPlayer(source)
+    if not Player then return end
+
+    local job = Player.PlayerData.job.name
+    if job ~= "police" and job ~= "ambulance" then
+        TriggerClientEvent('QBCore:Notify', source, "You don't have permission to use this.", "error")
+        return
+    end
+
+    local msg = table.concat(args, " ")
+    if msg == "" then
+        TriggerClientEvent('QBCore:Notify', source, "You must enter a message.", "error")
+        return
+    end
+
+    local header, color
+    if job == "police" then
+        header = "(Broadcast) [Gov - Police]"
+        color = { 0, 102, 204 } -- blue
+    else
+        header = "(Broadcast) [Government]"
+        color = { 255, 0, 0 } -- red
+    end
+
+    TriggerClientEvent('chat:addMessage', -1, {
+        color = color,
+        multiline = true,
+        args = { header, msg }
+    })
+end, false)
